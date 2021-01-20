059710ee216347121a04f8c073b3a0b349d694d1
Dennis Li <li.dennis.beta@gmail.com>
2021-01-19 20:26:27 -0800

    Update Calendar with Month End

--- CCalendar.h
+++ CCalendar.h
@@ -86,8 +86,29 @@ int zellersCongruence(int monthh, int yearr){
     final = final % 7;
     // cout << final << "=" << 1 << " + " << ((13 * (month + 1))/5) << " + " << year % 100 << " + " << (year % 100)/4 <<  " + " << (year/100)/4 << " + " << 5 * (year/100) << endl;
     // cout << days << endl;
-    cout << days[final] << endl;
+    // cout << days[final] << endl;
     return final;
+}
+int getMonthEnd(int month,int year){
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+        return 31;
+    }
+    else if (month == 4 || month == 6 || month == 9 || month == 11 ){
+        return 30;
+    }
+    else if (month == 2){
+        if (year % 4 != 0){
+            return 28;
+        }
+        else if (year % 100 != 0){
+            return 29;
+        }
+        else if (year % 400 != 0){
+            return 28;
+        }
+        return 29;
+    }
+    return -1;
 }
 
 bool printCalendar(int month, int year){
